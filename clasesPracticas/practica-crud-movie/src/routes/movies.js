bd9633fb0ae0e3ca37a092ecb7bce3573525b1ba
OmiCremer <87448345+OmiCremer@users.noreply.github.com>
2022-07-13 14:02:18 -0300

    corriendo

---
+++ clasesPracticas/practica-crud-movie/src/routes/movies.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const movieController = require("../controller/moviesController");
+
+
+
+//rutas de creación
+// router.???("???", movieController.???);
+// router.???("???", movieController.???);
+
+// //rutas para edición
+// router.???("???", movieController.???);
+// router.???("???", movieController.???);
+
+// //ruta para eliminar
+// router.???("???", movieController.???);
+
+module.exports = router;
