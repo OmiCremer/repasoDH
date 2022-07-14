2cac12f02e2e1e57f30494da205cbecf07b06710
OmiCremer <87448345+OmiCremer@users.noreply.github.com>
2022-07-13 22:57:28 -0300

    corriendo, rever CRUD

--- clasesPracticas/practica-crud-movie/src/controller/moviesController.js
+++ clasesPracticas/practica-crud-movie/src/controller/moviesController.js
@@ -1,9 +1,75 @@
-module.exports = {
-    list: (req, res)=>{
-        
-    },
-    detail: (req, res)=> {
-        res.render("movie-detail.ejs")
-    },
+let fs = require("fs");
+let path = require("path");
 
+let dataJson = fs.readFileSync(path.join(__dirname, "../data/movies.json"));
+let movies = JSON.parse(dataJson);
+
+function writeJson() {
+  let data = JSON.stringify(movies);
+  fs.writeFileSync(path.join(__dirname, "../data/movies.json"));
+}
+
+module.exports = {
+  list: (req, res) => {
+    //devolver una respuesta
+    res.render("movie-list", { movies });
+  },
+  detail: (req, res) => {
+    //Buscar una pelicula
+    let movieEcontrada = movies.find(function (movie) {
+      return movie.id == req.params.id;
+    });
+
+    //devolver una respuesta
+    res.render("movie-detail", { movie: movieEcontrada });
+  },
+  create: (req, res) => {
+    res.render("movie-create-form");
+  },
+  store: (req, res) => {
+    //crear peliculas
+    let newMovie = {
+      id: movies.length + 1,
+      title: req.body.title,
+      genre: req.body.genre,
+      rating: Number(req.body.rating),
+      image: req.file.filename,
+    };
+    movies.push(newMovie);
+
+    //modificar Json
+    writeJson();
+    res.render("/movies/list");
+  },
+  edit: (req, res) => {
+    //Buscar una pelicula
+    let movieEcontrada = movies.find(function (movie) {
+      return movie.id == req.params.id;
+    });
+    res.render("movie-edit-form", { movie: movieEcontrada });
+  },
+  update: (req, res) => {
+    //traer la pelicula que queremos modificar
+    let movie = movies.find(function (movie) {
+      return movie.id == req.params.id;
+    });
+
+    movie.title = req.body.title;
+    movie.rating = Number(req.body.rating);
+    movie.genre = req.body.genre;
+    movie.file = req.file ? req.file.filename : movie.file;
+
+    res.redirect("/movies/list");
+  },
+  destroy: (req, res) => {
+    let movieIndex = movies.findIndex(function (movie) {
+      return movie.id == req.params.id;
+    });
+
+    movies.splice(movieIndex, 1);
+
+    writeJson();
+
+    res.redirect("/movies/list");
+  },
 };
--- clasesPracticas/practica-crud-movie/src/routes/movies.js
+++ clasesPracticas/practica-crud-movie/src/routes/movies.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const movieController = require("../controller/moviesController");
+const configMulter = require("../middlewares/movieMulter");
 
+router.get("/list", movieController.list);
 
+router.get("/detail/:id", movieController.detail);
 
 //rutas de creación
-// router.???("???", movieController.???);
-// router.???("???", movieController.???);
+router.get("/create", movieController.create);
 
-// //rutas para edición
-// router.???("???", movieController.???);
-// router.???("???", movieController.???);
+router.post("/create", configMulter.single("image"), movieController.store);
 
-// //ruta para eliminar
-// router.???("???", movieController.???);
+//rutas para edición
+router.get("/edit/:id", movieController.edit);
+router.put("/edit/:id", configMulter.single("image"), movieController.update);
+
+//ruta para eliminar
+router.delete("/delete/:id", movieController.destroy);
 
 module.exports = router;
